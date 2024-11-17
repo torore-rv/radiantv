b8d7587697878135c0145c3405cb4c7355ca9702
torore-rv <ptororez@gmail.com>
2024-11-18 06:06:07 +0900

    - 배포 오류 수정

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { NovelLayout } from "@pages/NovelLayout";
 
 const Nav = () => {
     const router = useRouter();
