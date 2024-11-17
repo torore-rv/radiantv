7e357690f0be2072bfdfad2f8aaf8f8ff5976cf7
torore-rv <ptororez@gmail.com>
2024-11-18 07:34:38 +0900

    - nextConfig 추가

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' }
+    }
+  },  // 여기 콤마 추가
   eslint: {
     ignoreDuringBuilds: true
   }
