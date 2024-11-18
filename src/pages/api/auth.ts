466b25228c2d715031321146bc1514e5071b1412
torore-rv <ptororez@gmail.com>
2024-11-19 00:46:30 +0900

    - next auth 5.0.0-beta.3 버전 문법으로 수정

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,7 +1,7 @@
-import { AuthOptions } from 'next-auth'
+import type { NextAuthConfig } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-export const authOptions: AuthOptions = {
+export const authOptions: NextAuthConfig = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
