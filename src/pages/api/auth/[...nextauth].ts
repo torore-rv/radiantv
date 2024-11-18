3fdce80f1dd7219d552b075042f65b3810d06a28
torore-rv <ptororez@gmail.com>
2024-11-19 02:14:08 +0900

    - NEXTAUTH_SECRET 추가

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,7 +7,9 @@ const config = {
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
-    ]
+    ],
+    secret: process.env.NEXTAUTH_SECRET, // 필수 추가
+    debug: true, // 디버깅 활성화
 }
 
 export default NextAuth(config)
